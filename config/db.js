87d2cc950fd07a3ef5c488dfd600750f11727e2d
Eddy Hernandez <eddy.hernandez@rackspace.com>
2013-06-02 13:51:25 -0500

    update mongodb logging
    a hack since reconnected event never fires

--- config/db.js
+++ config/db.js
@@ -1,23 +1,39 @@
 module.exports = function(url) {
-	var fs = require('fs')
-	  , mongoose = require('mongoose');
-	
-	// connect to db
-	mongoose.connect(url);
+  var fs = require('fs'),
+  mongoose = require('mongoose'),
+  connected = false;
 
-	// Bootstrap models
-	var models_path = './app/models',
-	model_files = fs.readdirSync(models_path);
+  // connect to db
+  mongoose.connect(url);
 
-	model_files.forEach(function (file) {
-	  require('.' + models_path + '/' +file);
-	});
+  // Bootstrap models
+  var models_path = './app/models',
+  model_files = fs.readdirSync(models_path);
 
-	// log any db errors
-	mongoose.connection.on('error', function(err) {
-	  console.log(":::::::: WARNING: MONGODB ERRROR ::::::");
-	  console.log(err);
-	  console.log(":::::::::::::::::::::::::::::::::::::::");
-	});
+  model_files.forEach(function (file) {
+    require('.' + models_path + '/' +file);
+  });
+
+  mongoose.connection.on('open', function() {
+    console.log(":::::::: Express connection to mongodb opened :::::::: ");
+    connected = true;
+  });
+
+  mongoose.connection.on('connected', function () {
+    if (connected) {
+      console.log(":::::::: Express has reconnected to mongodb :::::::: ");
+    };
+  });
+
+  mongoose.connection.db.on('close', function() {
+    console.log(":::::::: Express connection to mongodb disconnected :::::::: ");
+  });
+
+  // log any db errors
+  mongoose.connection.on('error', function(err) {
+    console.log(":::::::: WARNING: MONGODB ERRROR ::::::");
+    console.log(err);
+    console.log(":::::::::::::::::::::::::::::::::::::::");
+  });
 
 };
